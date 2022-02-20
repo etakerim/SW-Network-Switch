3cc3612d7ee050e441b5881d02312fdac4b9fbbb
Miroslav Hájek <xhajekm@stuba.sk>
2022-02-21 00:33:35 +0100

    Change port naming

--- loopback.sh
+++ loopback.sh
@@ -1,24 +1,24 @@
 # sudo su
 
 #####CREATE###################################
-ip link add name sw-port0 type dummy
-ip link set sw-port0 up
-ip addr add 1.1.1.1/32 dev sw-port0 
+ip link add name P0 type dummy
+ip link set P0 up
+ip addr add 1.1.1.1/32 dev P0 
 
-ip link add name sw-port1 type dummy
-ip link set sw-port1 up
-ip addr add 1.1.1.2/32 dev sw-port1
+ip link add name P1 type dummy
+ip link set P1 up
+ip addr add 1.1.1.2/32 dev P1
 #######################################
 
 # ping 1.1.1.1
-# ip addr show sw-port1
+# ip addr show P1
 
 ######TEARDOWN##################################
-ip addr delete 1.1.1.1/32 dev sw-port0
-ip link set sw-port0 down
-ip link delete name sw-port0 type dummy
+ip addr delete 1.1.1.1/32 dev P0
+ip link set P0 down
+ip link delete name P0 type dummy
 
-ip addr delete 1.1.1.2/32 dev sw-port1
-ip link set sw-port1 down
-ip link delete name sw-port1 type dummy
+ip addr delete 1.1.1.2/32 dev P1
+ip link set P1 down
+ip link delete name P1 type dummy
 #######################################
